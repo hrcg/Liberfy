0d922e743f0862abf33af3a0b0dbd9f51e9ce9c8
hrcg <perrandor@gmail.com>
2021-01-22 17:01:51 +0100

    implemented routing on new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { WishlistComponent } from './wishlist/wishlist.component';
 import { LoginComponent } from './authComponents/login/login.component';
 import { RegisterComponent } from './authComponents/register/register.component';
 import { UsersComponent } from './authComponents/users/users.component';
@@ -9,7 +8,6 @@ import { UsersComponent } from './authComponents/users/users.component';
 const routes: Routes = [
   // { path: '**', redirectTo: '/home'},
   { path: 'home', component: HomeComponent},
-  { path: 'wishlist', component: WishlistComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'users', component: UsersComponent}
