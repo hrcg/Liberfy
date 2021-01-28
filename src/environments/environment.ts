7a791a43b10ff4018efc2180c18d718cda369d95
hrcg <perrandor@gmail.com>
2021-01-28 15:23:29 +0100

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
     firebase: {
-    apiKey: "AIzaSyDlnOB-diEP8Q6KayisW9qeqqJ9WzIk8C4",
-    authDomain: "liberfy-e95f6.firebaseapp.com",
-    projectId: "liberfy-e95f6",
-    storageBucket: "liberfy-e95f6.appspot.com",
-    messagingSenderId: "455918102813",
-    appId: "1:455918102813:web:e18c227b725a1af9f799f4",
-    measurementId: "G-DX7P2HNKF5"
+    apiKey: 'AIzaSyDlnOB-diEP8Q6KayisW9qeqqJ9WzIk8C4',
+    authDomain: 'liberfy-e95f6.firebaseapp.com',
+    projectId: 'liberfy-e95f6',
+    storageBucket: 'liberfy-e95f6.appspot.com',
+    messagingSenderId: '455918102813',
+    appId: '1:455918102813:web:e18c227b725a1af9f799f4',
+    measurementId: 'G-DX7P2HNKF5'
 }
 
 };
