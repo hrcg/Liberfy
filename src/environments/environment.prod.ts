32b10f9f8f4aaa2423423a67b796d3a3a5f55ec7
hrcg <perrandor@gmail.com>
2021-02-07 13:34:24 +0100

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDlnOB-diEP8Q6KayisW9qeqqJ9WzIk8C4",
+    authDomain: "liberfy-e95f6.firebaseapp.com",
+    projectId: "liberfy-e95f6",
+    storageBucket: "liberfy-e95f6.appspot.com",
+    messagingSenderId: "455918102813",
+    appId: "1:455918102813:web:e18c227b725a1af9f799f4",
+    measurementId: "G-DX7P2HNKF5"
+  }
 };
